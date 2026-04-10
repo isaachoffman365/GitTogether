f117918088e36a01b19b672e0b97fb7bf4173ce6
valkyrie_pilot <valk@randomairborne.dev>
2026-04-10 18:48:26 -0500

    add content scripts and such

---
+++ build/background-script.js
@@ -0,0 +1,7 @@
+// background-script.ts
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id || -1 },
+    files: ["build/content-script.js"]
+  });
+});
---
+++ build/content-script.js
@@ -0,0 +1,16 @@
+// content-script.ts
+async function main() {
+  const captionsNodeList = document.querySelectorAll("caption");
+  const captions = Array.from(captionsNodeList);
+  const enrolledCoursesCaption = captions.find((v) => v.textContent.includes("My Enrolled Courses"));
+  if (!enrolledCoursesCaption) {
+    alert("Could not find table.");
+    return;
+  }
+  const rawTable = enrolledCoursesCaption.parentNode;
+  const tBody = rawTable?.querySelectorAll("tbody").item(0);
+  for (const row of tBody?.childNodes || []) {
+    console.log(row.childNodes.item(1).textContent);
+  }
+}
+main().then(() => {});
