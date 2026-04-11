a6a4674e01e009e8ecaf9300105c41d429d02d30
isaachoffman365 <isaachoffman365@gmail.com>
2026-04-11 10:34:09 -0500

    Updated parsing of course name, dates, and times to create event in GCal; unfinished

--- createics.ts
+++ createics.ts
@@ -1,3 +1,11 @@
+    //TODO: Make sure code works (there seem to be problems with "course" and subtracting
+    //      startDate from endDate). Primary function currently starts on line 48.
+    //TODO: Get each created event to output onto a string/file so that we can get an ICS file
+    //      as a result of running the program.
+    //NOTES: All the commented-out code can be ignored, they are just helpful templates/ideas 
+    //       for creating iCalendar events and printing an ICS-formatted string
+
+
 //Read in File
 // const path = "/path/to/file.txt";
 // const file = Bun.file(path);
@@ -28,63 +36,83 @@ import * as ics from 'ics';
 //   ]
 // }
 
-ics.createEvent(event, (error, value) => {
-  if (error) {
-    console.log(error)
-    return
-  }
+// ics.createEvent(event, (error, value) => {
+//   if (error) {
+//     console.log(error)
+//     return
+//   }
 
-  console.log(value)
-})
+//   console.log(value)
+// })
 
 function toWhatever(classes: CalTransfer[]) {
     for (const course of classes) {
         for (const time of course) {
             const [days, duration, location] = course.times[time].split('|')
-            let name = course.name 
+            let name = course.name
+            let startDate = course.start
+            let endDate = course.end
+            const classDuration = endDate - startDate
 
             const [startTime, startMeridian, hyphen, endTime, endMeridian] = duration.split(' ')
             
             // Split startTime and endtime into the hours and minutes to input into ICS "duration" field
-            let totalDuration = new Date(Math.abs(endTime - startTime)) //times are currently strings!
+            // let totalDuration = new Date(Math.abs(endTime - startTime)) //times are currently strings!
 
             const event = {
-            start: [2026, 5, 30, 6, 30], //year, month, day, hour, min //CHANGE to make dynamic
-            duration: { hours: 3, minutes: 30 },
-            title: 'Carl\'s Calendar',
+            start: [startDate], //year, month, day, hour, min //CHANGE to make dynamic
+            duration: { classDuration },
+            title: name,
             description: '',
             location: location,
             url: '',
-            geo: { },
-            categories: [''],
+            geo: {},
+            categories: [],
             status: '',
             busyStatus: 'BUSY',
-            organizer: { },
-            attendees: [{ }]
+            organizer: {},
+            attendees: [{}]
             }
         }   
     }
 
-
-
-    // return  "BEGIN:VCALENDAR\n" + 
-    //         "VERSION:2.0\n" +
-    //         "CALSCALE:GREGORIAN\n" +
-    //         "PRODID:carl/ics\n" +
-    //         "METHOD:PUBLISH\n" +
-    //         "X-PUBLISHED-TTL:PT1H\n" +
-    //         "BEGIN:VEVENT\n" +
-    //         "UID:a6PdgV6Pxt4LVLNvVL_pJ\n" + //unique id UID:20260410T120000-uniqueid@yourdomain.com
-    //         // "UID:" + Date.now + "@carleton.edu" +
-    //         "SUMMARY:Dinner\n" +
-    //         "DTSTAMP:20260410T232755Z\n" +
-    //         "DTSTART:20180115T123000Z\n" +
-    //         "DESCRIPTION:Nightly thing I do\n" +
-    //         "X-MICROSOFT-CDO-BUSYSTATUS:FREE\n" +
-    //         "DURATION:PT50M\n" +
-    //         "END:VEVENT\n" +
-    //         "END:VCALENDAR\n"
+    //TEMPLATE BELOW (what Isaac started with before adapting to Workday schedule:)
+    // const event = {
+    //   start: [2018, 5, 30, 6, 30], //year, month, day, 6?, 30?
+    //   duration: { hours: 6, minutes: 30 },
+    //   title: 'Carl\'s Calendar',
+    //   description: 'Annual 10-kilometer run in Boulder, Colorado',
+    //   location: 'Folsom Field, University of Colorado (finish line)',
+    //   url: 'http://www.bolderboulder.com/',
+    //   geo: { lat: 40.0095, lon: 105.2669 },
+    //   categories: ['10k races', 'Memorial Day Weekend', 'Boulder CO'],
+    //   status: 'CONFIRMED',
+    //   busyStatus: 'BUSY',
+    //   organizer: { name: 'Admin', email: 'Race@BolderBOULDER.com' },
+    //   attendees: [
+    //     { name: 'Adam Gibbons', email: 'adam@example.com', rsvp: true, partstat: 'ACCEPTED', role: 'REQ-PARTICIPANT' },
+    //     { name: 'Brittany Seaton', email: 'brittany@example2.org', dir: 'https://linkedin.com/in/brittanyseaton', role: 'OPT-PARTICIPANT' }
+    //   ]
+    // }
 }
+
+// return  "BEGIN:VCALENDAR\n" + 
+//         "VERSION:2.0\n" +
+//         "CALSCALE:GREGORIAN\n" +
+//         "PRODID:carl/ics\n" +
+//         "METHOD:PUBLISH\n" +
+//         "X-PUBLISHED-TTL:PT1H\n" +
+//         "BEGIN:VEVENT\n" +
+//         "UID:a6PdgV6Pxt4LVLNvVL_pJ\n" + //unique id UID:20260410T120000-uniqueid@yourdomain.com
+//         // "UID:" + Date.now + "@carleton.edu" +
+//         "SUMMARY:Dinner\n" +
+//         "DTSTAMP:20260410T232755Z\n" +
+//         "DTSTART:20180115T123000Z\n" +
+//         "DESCRIPTION:Nightly thing I do\n" +
+//         "X-MICROSOFT-CDO-BUSYSTATUS:FREE\n" +
+//         "DURATION:PT50M\n" +
+//         "END:VEVENT\n" +
+//         "END:VCALENDAR\n"
 
 //Write an iCalendar file:
 // const { writeFileSync } = require('fs')
@@ -119,11 +147,10 @@ function toWhatever(classes: CalTransfer[]) {
 // END:VCALENDAR
 //
 
-
 /*
 You cannot use fs in Frontend libraries like React so you rather import a module to save files 
 to the browser as follow [  import { saveAs } from 'file-saver'; // For saving the file in the browser]
 const blob = new Blob([value], { type: 'text/calendar' });
         saveAs(blob, `${title}.ics`);
 */
-})
+// })
